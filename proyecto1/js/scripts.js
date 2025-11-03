de284d03c1e1a4e57aab2013a2d3dcd2b6adfadd
Jesús <jtipian@gmail.com>
2025-11-03 01:33:26 +0100

    Ajustes visuales en Luz&Sombra

--- proyecto1/js/scripts.js
+++ proyecto1/js/scripts.js
@@ -30,17 +30,62 @@ const galeria = document.getElementById("galeria");
 const contacto = document.getElementById("contacto");
 const body = document.body;
 
+//Pantalla de carga
 body.classList.add("noScroll");
+
 setTimeout(() => {
     sectionInicial.classList.add("ocultar");
     hero.classList.remove("ocultar");
+    document.querySelector('[data-section="hero"]').classList.add("linkActive");
     about.classList.remove("ocultar");
     galeria.classList.remove("ocultar");
     contacto.classList.remove("ocultar");
     btnToggle.classList.remove("ocultar")
     body.classList.remove("noScroll");
-
 }, 3000);
+
+
+//===========================A partir de LG min-width: 992px =========================================
+
+const lg = window.matchMedia("(min-width: 992px)");
+const sections = document.querySelectorAll("section");
+
+if (lg.matches) {
+
+    setTimeout(() => {
+        sectionInicial.classList.add("ocultar");
+        hero.classList.remove("ocultar");
+        about.classList.add("ocultar");
+        galeria.classList.add("ocultar");
+        contacto.classList.add("ocultar");
+        btnToggle.classList.add("ocultar");
+        body.classList.remove("noScroll");
+    }, 3000);
+
+    const links = document.querySelectorAll(".linkNav");
+
+    links.forEach(link => {
+        const activateLink = () => {
+            // Quita la clase activa del resto
+            links.forEach(l => l.classList.remove("linkActive"));
+
+            // Añade la clase activa al actual
+            link.classList.add("linkActive");
+
+            // Oculta todas las secciones
+            sections.forEach(sec => sec.classList.add("ocultar"));
+
+            // Muestra la sección correspondiente
+            const target = document.getElementById(link.dataset.section);
+            target.classList.remove("ocultar");
+        };
+
+        // Activa tanto con click como con focus
+        link.addEventListener("click", activateLink);
+        link.addEventListener("focus", activateLink);
+    });
+}
+
 
 
 
